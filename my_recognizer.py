b835402eccbea3966ce87234d582ce18ece4deb7
Scott W. Davis <zscottywd@msn.com>
2017-05-24 10:39:48 -0700

    Add files via upload

--- my_recognizer.py
+++ my_recognizer.py
@@ -4,7 +4,6 @@ from asl_data import SinglesData
 
 def recognize(models: dict, test_set: SinglesData):
     """ Recognize test word sequences from word models set
-
    :param models: dict of trained models
        {'SOMEWORD': GaussianHMM model object, 'SOMEOTHERWORD': GaussianHMM model object, ...}
    :param test_set: SinglesData object
@@ -20,6 +19,22 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    all_sequences = test_set.get_all_sequences()
+    all_Xlengths = test_set.get_all_Xlengths()
+    for sequence in all_sequences:
+        probability = {}
+        X, length = all_Xlengths[sequence]
+        for word_model, model in models.items():
+            try:
+                score = model.score(X, length)
+                probability[word_model] = score
+            except:
+                score = -float("inf")
+                probability[word_model] = score
+        probabilities.append(probability)
+        values = list(probability.values())
+        keys = list(probability.keys())
+        guesses.append(keys[values.index(max(values))])
+    return probabilities, guesses
+
+
